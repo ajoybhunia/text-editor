01d8b59c694b2f5c6459c4acdfcf8c91b5c4fa4a
Ajoy Bhunia <ajoybhunia@Ajoys-MacBook-Pro.local>
2026-02-21 01:56:48 +0530

    Modifies main.js
    * opens a file if file is already exists
    * does not create any file initially
    * creates a file when it needs to write on it
    * handles error gracefully when it tries to open a directory

--- editor/main.js
+++ editor/main.js
@@ -1,7 +1,7 @@
 import { Editor } from "./src/editor.js";
 import { Terminal } from "./src/terminal.js";
 
-const main = async (filePath) => {
+/* const main = async (filePath) => {
   const file = await Deno.open(filePath, {
     read: true,
     write: true,
@@ -19,6 +19,38 @@ const main = async (filePath) => {
 
   if (info.shouldWrite) await Deno.writeFile(filePath, info.data);
   await Terminal.clear();
+};
+
+await main(Deno.args[0]); */
+
+const main = async (filePath) => {
+  try {
+    const { isFile, size } = await Deno.stat(filePath);
+
+    if (isFile) {
+      const file = await Deno.open(filePath, { read: true });
+
+      const buffer = new Uint8Array(size);
+      const n = await file.read(buffer);
+      file.close();
+
+      const editor = new Editor(buffer.subarray(0, n));
+      const info = await editor.run();
+
+      if (info.shouldWrite) await Deno.writeFile(filePath, info.data);
+      await Terminal.clear();
+    }
+
+    await Terminal.write(
+      new TextEncoder().encode("Error: Failed to open a directory!"),
+    );
+  } catch {
+    const editor = new Editor(new Uint8Array(0));
+    const info = await editor.run();
+
+    if (info.shouldWrite) await Deno.writeFile(filePath, info.data);
+    await Terminal.clear();
+  }
 };
 
 await main(Deno.args[0]);
