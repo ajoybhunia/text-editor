c90cfde29ea26f9ae1ed990b34cfd3f47061ac4e
Ajoy Bhunia <ajoybhunia@Ajoys-MacBook-Pro.local>
2026-03-01 23:32:11 +0530

    modifies entry point

--- editor/src/core/editor.js
+++ editor/src/core/editor.js
@@ -1,6 +1,6 @@
-import { Terminal } from "../terminal/terminal.js";
 import Cursor from "../domain/cursor.js";
 import TextBuffer from "../domain/text_buffer.js";
+import { Terminal } from "../terminal/terminal.js";
 import { render } from "../terminal/terminal_renderer.js";
 import { computeCursorPos } from "../utils/compute_cursor.js";
 import { KEYS } from "../config/keys.js";
